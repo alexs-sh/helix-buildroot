d8fbdb60987d27364aa6e29f4c15fcda0e356948
Alexander Shirokov <shirokovalexs@gmail.com>
2025-07-23 10:44:08 +0200

    build:allow debug build

--- build-img.sh
+++ build-img.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 DEFCONFIG=qemu_aarch64_virt_defconfig
+MODE="release"
 
 if [ "$#" -ge 1 ]; then
    DEFCONFIG=$1
+fi
+
+if [ "$#" -gt 1 ]; then
+   MODE=$2
 fi
 
 readonly BR_CONFIG=buildroot/configs/"$DEFCONFIG"
@@ -13,6 +18,13 @@ set -e
 
 echo "Add config $DEFCONFIG"
 cp configs/"$DEFCONFIG" "$BR_CONFIG"
+
+
+if [ "$MODE" == "debug" ]; then
+  echo "Enable debug"
+  echo "BR2_ENABLE_DEBUG=y" >> "$BR_CONFIG"
+fi
+
 
 echo "Apply config $DEFCONFIG"
 cd buildroot
